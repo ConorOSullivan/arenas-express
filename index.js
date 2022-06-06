25306839c35472c1bc4f085eabf62de50532e1eb
Conor O'Sullivan <conorosullivan@fb.com>
2022-06-06 14:19:50 -0700

    adding updates for cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 8080;
 
 app.listen(
@@ -7,6 +8,7 @@ app.listen(
     () => console.log(`its alive on http://localhost:${PORT}`)
 )
 app.use(express.json());
+app.use(cors());
 app.get('/', (req, res) => {
     res.status(200).send({
         screen: 'home screen'
