f6af576059f76edda4c68c64294d4b813286fb73
ConorOSullivan <cosullivan66@gmail.com>
2022-06-06 13:06:09 -0700

    Add files via upload

---
+++ routes/users.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/:id', function(req, res, next) {
+  const { id } = req.params;
+  res.send(`Here is the page for user #${id}`);
+});
+
+module.exports = router;
